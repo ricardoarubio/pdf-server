84281c90912e7e7f468dca5bad961edd2998b0fa
ricardoarubio <ricardoarubio823@gmail.com>
2021-01-28 09:35:58 -0600

    commit

---
+++ dist/pdf-server/index.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Server_1 = __importDefault(require("./classes/Server"));
+const router_1 = __importDefault(require("./routes/router"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const server = new Server_1.default();
+//bodyParser
+server.app.use(body_parser_1.default.urlencoded({ extended: true }));
+server.app.use(body_parser_1.default.json());
+//cors
+server.app.use(cors_1.default({ origin: true, credentials: true }));
+//rutas
+server.app.use('/', router_1.default);
+server.start(() => {
+    console.log(`servidor iniciado en el puerto ${server.port}`);
+});
---
+++ dist/pdf-server/routes/router.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const router = express_1.Router();
+router.get('/mensajes', (req, res) => {
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+    res.json({
+        ok: true,
+        cuerpo,
+        de
+    });
+});
+router.post('/mensajes/:id', (req, res) => {
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+    res.json({
+        ok: true,
+        cuerpo,
+        de
+    });
+});
+exports.default = router;
